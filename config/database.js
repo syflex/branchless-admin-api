87eba11610c0b7d0e6ebea0e5d5168ea1d24f64b
Simon Moses Onazi <syflex360@gmail.com>
2020-06-09 09:16:41 +0100

    updated db

--- config/database.js
+++ config/database.js
@@ -4,13 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: env('DATABASE_CLIENT', 'mysql'),
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'branchless-admin'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: env('${process.env.DATABASE_CLIENT}', 'mysql'),
+        host: env('${process.env.DATABASE_HOST}', '127.0.0.1'),
+        port: env.int('${process.env.DATABASE_PORT}', 3306),
+        database: env('${process.env.DATABASE_NAME}', 'branchless-admin'),
+        username: env('${process.env.DATABASE_USERNAME}', 'root'),
+        password: env('${process.env.DATABASE_PASSWORD}', ''),
+        ssl: env.bool('${process.env.DATABASE_SSL}', false),
       },
       options: {}
     },
